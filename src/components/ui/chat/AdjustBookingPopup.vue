2cf0e673a6e24b7521409a487ebb272ff43133ad
prosenjit-deb <prosenjitd.wt@gmail.com>
2026-07-20 17:54:02 +0530

    session duration Ui Update

--- src/components/ui/chat/AdjustBookingPopup.vue
+++ src/components/ui/chat/AdjustBookingPopup.vue
@@ -107,7 +107,7 @@
               >
                 <span aria-hidden="true">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-  <path d="M5 12H19" stroke="#5549FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M5 12H19" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
                 </span>
               </button>
@@ -122,7 +122,7 @@
                 @contextmenu.prevent
               >
                 <span aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-  <path d="M12 5V19M5 12H19" stroke="#5549FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M12 5V19M5 12H19" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg></span>
               </button>
               </div>
